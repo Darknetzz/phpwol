6e8f67af845af3af259627f1fb384a497f597bac
Darknetzz <github@roste.org>
2020-10-24 15:37:34 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -5,4 +5,15 @@
   $id = mysqli_real_escape_string($sqlcon, $_GET['id']);
   $getcomputer = "SELECT * FROM computers WHERE id = '$id'";
   $getcomputer = mysqli_query($sqlcon, $getcomputer);
+    while ($row = $getcomputer->fetch_assoc()) {
+    echo "
+    <table class='table-default'>
+    <tr><th>Hostname</th><th>IP</th><th>MAC-address</th></th></tr>
+    <tr>
+        <td><input type='text' name='hostname' value='$row[hostname]' class='form-control'></td>
+        <td><input type='text' name='ip' value='$row[ip]' class='form-control'></td>
+        <td><input type='text' name='mac' value='$row[mac]' class='form-control'></td>
+    </table>";
+    }
+
 ?>
